ebd4b60aa2fc712effa1ce34deb9e5347333748f
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2024-12-27 02:02:08 +0530

    err resolve

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(express.json(
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser());
 app.use(cors({
-    origin:["http://localhost:5173","http://127.0.0.1:5173"],
+    origin:["https://music-studio-rjkw.onrender.com"],
     credentials:true,
     methods:["GET","POST","PUT","DELETE"]
 }))
