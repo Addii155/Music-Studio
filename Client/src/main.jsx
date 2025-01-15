75537dca084de4c2a7a113c62bc7587d4254be46
Aditya Gupta <148351333+AaDiTYAKumaR1@users.noreply.github.com>
2025-01-15 15:38:48 +0530

    err solved

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,19 +5,14 @@ import './index.css';
 import {Provider} from "react-redux";
 import store from './redux/store.js';
 import { Toaster } from 'react-hot-toast';
-import { ThemeProvider } from "@/components/theme-provider" 
-import { Play } from 'lucide-react';
-import  dotenv from 'dotenv'
 
  
 import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId='238683043090-k2okjt6vhfqphd7i62d34fgn6342jb0r.apps.googleusercontent.com'>
     <Provider store={store}>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <App />
     <Toaster/>
-    </ThemeProvider>
     </Provider>
     </GoogleOAuthProvider>
    ,
